1d356f09a97c696c49ed4d2a52b510192377e52c
Mohammed Seoud <m.seoud42@gmail.com>
2023-06-29 03:48:20 +0300

    FirstCommit

---
+++ src/Component/FavoriteContacts.jsx
@@ -0,0 +1,9 @@
+const FavoriteContacts = ()=>{
+
+    return(
+        <div>
+            <button className="btn btn-secondary form-control"> Favorite Contact </button>
+        </div>
+    )
+}
+export default FavoriteContacts
---
+++ src/Component/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import classes from './Footer.module.css'
+
+const Footer = ()=>{
+
+    return(
+        <footer className={classes.footer}>
+            Happy Coding @ContactOpedia
+        </footer>
+    )
+}
+export default Footer;
